9deed28556e3f9ef3017493ce77d8d369e540d4c
o.shott <os@jetstat.io>
2021-04-24 10:21:37 +0300

    New

---
+++ Homework8/Homework8/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework8
+{
+    public class Class1
+    {
+        public void WriteString(string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
---
+++ Homework8/Homework8_enter/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Homework8;
+
+namespace Homework8_enter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 test = new Class1();
+            test.WriteString("Hello World");
+        }
+    }
+}
